246a32939cd3c06905ff4d24ae4295481a1e9ba5
Mohammad Al Khatib <Mohammad.AlKhatib@fastlink.com.jo>
2023-03-11 04:06:27 +0300

    All New

--- server.js
+++ server.js
@@ -4,17 +4,20 @@ require('dotenv').config()
 const bp = require('body-parser')
 const PORT=process.env.PORT || 8000;
 
+
 // SessionToken
 var session = require('express-session')
-// app.set('trust proxy', 1) // trust first proxy
-// app.use(session({
-//   secret: 'kAjllfhlTg6zvvW52D6f',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: true }
-// }))
-app.use(session({ secret: 'kAjllfhlTg6zvvW52D6f', cookie: { maxAge: null }}))
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'kAjllfhlTg6zvvW52D6f',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+// app.use(session({ secret: 'kAjllfhlTg6zvvW52D6f', cookie: { maxAge: null }}))
 //
+app.use(express.static('./public')); // Public file
+app.set('view engine', 'ejs');
 
 
 
@@ -38,6 +41,9 @@ const client = new pg.Client(process.env.DATABASE_URL);
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
+// Vars
+var status ="";
+// 
 
 //Error Handler
 process.on
@@ -52,14 +58,16 @@ process.on
 );
 
 app.get('/', home)
+app.get('/test',test )
 app.get('/newuser', newUser)
 app.get('/show', getUser)
-app.get('/login', login)
+app.get('/login', checkLogin)
 app.get('/logout', logout)
 app.post('/createdebt', createDebt)
-app.get('/acceptdebt', acceptDebt)
+app.post('/acceptdebt', acceptDebt)
 app.get('/debtbyme', debtByMe)
 app.get('/debtforme', debtForMe)
+app.get('/create', createDebtview)
 
 
 
@@ -70,18 +78,31 @@ app.get('/mysession',function(req,res){
 });
 // 
 
-function home(req, res) {
-res.sendFile('index.html', {root: __dirname});
+function test(req,res){
+  res.render('pages/index', {
+    status: status
+  });
 }
 
-function login(req, res) {
+function home(req, res) {
+  if(req.session.username==null){
+    res.render('pages/login', {
+      status: status
+    });
+    status ="";
+  }else{
+    res.redirect('../debtForMe');
+  }
+}
+
+function checkLogin(req, res) {
   if(req.session.username!=null){
     req.session.reload(function(err) {
       // session updated
     })
   }
   try {
-    if(req.query.name==undefined||req.query.pass==undefined){res.status(500).send("Missing data");}
+    if(req.query.name==undefined||req.query.pass==undefined){res.status(200).send(JSON.parse('["Missing Data"]'));}
     else{
     let UserName = req.query.name;
     let password = req.query.pass;
@@ -94,31 +115,36 @@ function login(req, res) {
         req.session.email = data.rows[0].email;
         req.session.phone = data.rows[0].phone;
         
-        res.status(200).send("Login Successfully");
+        // console.log(req.session.id);
+
+        // res.status(200).send(JSON.parse('["Login Successfully"]'));
             
       }
       else{
-        res.status(500).send("Wrong Username or Password");
+        // res.status(200).send(JSON.parse('["Wrong Username or Password"]'));
+        status = "Wrong Username Or Password"
       }
+      res.redirect('../')
       });
     }
   
   } catch (error) {
-    res.status(500).send('Sorry, something went wron' + error);
+    res.status(200).send('Sorry, something went wron' + error);
   }
 }
 
 function logout(req,res){
   req.session.destroy();
-  res.end("Logout Done");
-
+  // res.end("Logout Done");
+  // res.status(200).send(JSON.parse('["Logout Is Done"]'));
+  res.redirect('../')
 }
 
 function newUser(req, res) {
   try {
-    if(req.query.name==undefined||req.query.email==undefined||req.query.pass==undefined||req.query.phone==undefined){res.status(500).send("Missing data");}
-    else if(req.query.pass.length<8){res.status(500).send("Password less than 8 characters");}
-    else if(req.query.phone.length<10){res.status(500).send("Mobile number less than 10 characters");}
+    if(req.query.name==undefined||req.query.email==undefined||req.query.pass==undefined||req.query.phone==undefined){res.status(200).send("Missing data");}
+    else if(req.query.pass.length<8){res.status(200).send("Password less than 8 characters");}
+    else if(req.query.phone.length<10){res.status(200).send("Mobile number less than 10 characters");}
     else{
     let UserName = req.query.name;
     let email = req.query.email;
@@ -126,7 +152,7 @@ function newUser(req, res) {
     let phone = req.query.phone;
     
     client.query('SELECT * FROM Users WHERE userName = ($1) OR email = ($2)',[UserName,email]).then(data => {
-      if(data.rows.length != 0){res.status(500).send("Username or email exists");}
+      if(data.rows.length != 0){res.status(200).send("Username or email exists");}
         else{
           let sqlQuery = 'insert into Users(userName,email,password,phone) values ($1,$2,$3,$4)';
           let value = [UserName,email,password,phone];
@@ -137,7 +163,7 @@ function newUser(req, res) {
       });
   }
   } catch (error) {
-    res.status(500).send('Sorry, something went wron' + error);
+    res.status(200).send('Sorry, something went wron' + error);
   }
 }
 
@@ -146,66 +172,70 @@ function getUser(req, res) {
     client.query(sqlQuery).then(data => {
       // console.log(data.rows);
       res.status(200).send(data.rows);
-      res.status(200).send("ALL USERS");
       });
+}
+function createDebtview(req,res){
+  if(req.session.username==null){res.redirect('../');}else{
+    res.render('pages/createDebt',{
+      status: ""
+    });
+  }
 }
 
 function createDebt (req,res){
-  if(req.session.username==null){res.status(500).send("Login first");}
-  else if(!req.body.title||!req.body.descr||!req.body.amount||!req.body.createdto||!req.body.duedate){res.status(500).send("Missing data");}
+  if(req.session.username==null){res.redirect('../');}
+  else if(!req.body.title||!req.body.descr||!req.body.amount||!req.body.createdto||!req.body.duedate){res.redirect('/create');}
   else{
   let sqlQuery = 'insert into debt (status,title,descr,amount,createdby,createdto,creationdate,duedate,completiondate) values ($1,$2,$3,$4,$5,$6,$7,$8,$9)';
   let value = [0,req.body.title,req.body.descr,req.body.amount,req.session.UserId,req.body.createdto,TodDate,req.body.duedate,null];
   client.query(sqlQuery, value).then(data => {
   });
-  res.status(200).send("Registration Is Done");
+  res.render('pages/createDebt',{
+    status: "done"
+  });
   }
 }
 
 function acceptDebt(req,res){
-  if(req.session.username==null){res.status(500).send("Login first");}
-  else if(req.query.DebtId==undefined){res.status(500).send("Missing data");}
+  if(req.session.username==null){res.redirect('../');}
+  else if(req.body.DebtId==undefined){res.redirect('../');}
   else{
-    client.query('SELECT * FROM debt WHERE id = ($1) and createdto = ($2)',[req.query.DebtId,req.session.UserId]).then(data => {
+    client.query('SELECT * FROM debt WHERE id = ($1) and createdto = ($2)',[req.body.DebtId,req.session.UserId]).then(data => {
       if(data.rows.length == 1){
-        client.query('UPDATE debt SET status = ($1) WHERE id = ($2) and createdto = ($3)',[true,req.query.DebtId,req.session.UserId]).then(data => {
-          res.status(200).send("Accepted Successfully");
+        client.query('UPDATE debt SET status = ($1) WHERE id = ($2) and createdto = ($3)',[true,req.body.DebtId,req.session.UserId]).then(data => {
+          res.redirect('../');
         });
       }
       else{
-        res.status(500).send("Permission Denied");
+        res.redirect('../');
       }
       });
 
   }
 }
 
+
 function debtByMe(req,res){
-  if(req.session.username==null){res.status(500).send("Login first");}
+  if(req.session.username==null){res.redirect('../');}
   else{
     client.query('SELECT * FROM debt WHERE createdby = ($1)',[req.session.UserId]).then(data => {
-      res.status(200).send(data.rows);
+      res.render('pages/debtByMe', {
+        data: data.rows
+      });
     });
   }
 }
 
 function debtForMe(req,res){
-  if(req.session.username==null){res.status(500).send("Login first");}
+  if(req.session.username==null){res.redirect('../');}
   else{
     client.query('SELECT * FROM debt WHERE createdto = ($1)',[req.session.UserId]).then(data => {
-      res.status(200).send(data.rows);
+      res.render('pages/debtForMe', {
+        data: data.rows
+      });
     });
   }
 }
-
-
-
-
-
-
-
-
-
 
 
 
